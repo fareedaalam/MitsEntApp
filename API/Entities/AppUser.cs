3433f13d6cb5c306fb9204d4dd069480f958e948
Your Name <you@example.com>
2022-09-01 16:39:55 +0530

    First Commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public long Id { get; set; }
+        public String UserName { get; set; }
+
+    }
+}
